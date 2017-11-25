e41de0cc49226a112355a40d247a54aaae5a78f4
Bang Tran <bangotran21@gmail.com>
2017-11-25 10:53:30 -0800

    moved audio and got sound in

--- drum_machine/src/components/track.js
+++ drum_machine/src/components/track.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import Tone from 'tone'
+import tone, {Player} from 'tone'
+import samples from './samples.json'
+
+const sounds = samples.map(sample=>require(`../audio/${sample}.wav`));
+
+// let synth = new Tone.Synth().toMaster();
+
+//play a middle 'C' for the duration of an 8th note
+// synth.triggerAttackRelease("C4", "8n");
+
+//start the note "D3" one second from now
+// synth.triggerAttack("D3", "+1");
+
+console.log(sounds);
+let player = new Player(sounds[21]).toMaster();
+//play as soon as the buffer is loaded
+player.autostart = true;
+
+
+// const p = new Player({
+// 	"url" : "../audio/kick-808.wav",
+// 	"autostart" : true,
+// }).toMaster();
 
 
 const Track = (props) => {
   return (
       <div>
-        <button value="Play">
+        <button value="Play">Play
 
         </button>
 
